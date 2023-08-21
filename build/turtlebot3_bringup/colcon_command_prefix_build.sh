249448b6ee63b179624dd02b07ea755cc618a404
Talzoor <talzoor@gmail.com>
2023-08-22 00:19:58 +0300

    upload from RPI

--- build/turtlebot3_bringup/colcon_command_prefix_build.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/t-pi/TB3_RPI4_WS/install/turtlebot3_description/share/turtlebot3_description/package.sh"
-. "/home/t-pi/TB3_RPI4_WS/install/turtlebot3_node/share/turtlebot3_node/package.sh"
